e926588c90c024edc4735673b7b7b423732d340a
Felle00 <32451795+Felle00@users.noreply.github.com>
2018-04-13 14:45:05 +0200

    First commit
    Del 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//Including functions
+require ('resources/functions/functions.php');
+//Create function for the Subpages
+/*
+- Start
++- Läsa inlägg som News KLart
++- Kommentera på inlägg. (Krävs inlogg) KLart
++- Gilla inläggen. KLart
+- About
+- Posts (Syns bara för inloggade)
++- Man skall kunna posta inlägg.
++- Redigera inlägg
+- Profil
++- Redigera information.
+++- Namn, Bild, Civilstatus osv.
+*/
+if (){
+}
+include ('resources/views/layout.php');
+ ?>
